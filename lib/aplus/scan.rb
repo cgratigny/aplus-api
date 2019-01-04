4132b22daffaa21da5fde99eb3a3d82d5bbe1ea1
Chris Gratigny <chrisg@ibethel.org>
2019-01-04 13:56:29 -0800

    return objects and add attributes

--- lib/aplus/scan.rb
+++ lib/aplus/scan.rb
@@ -1,17 +1,20 @@
 module AplusApi
   class Scan
     include ActiveModel::Model
-    attr_accessor :tag, :whenUtc, :location, :entry, :type
+    attr_accessor :tag, :whenUtc, :location, :entry, :type, :studentID, :whenCreatedUtc
 
     class << self
       def all(starting_at = 1.week.ago)
-        AplusApi::Connection.new.get("scans", { "fromDateUtc" => starting_at.to_date.to_s })
+        scans = []
+        AplusApi::Connection.new.get("scans", { "fromDateUtc" => starting_at.to_date.to_s }).each do |record|
+          scans << AplusApi::Scan.new(record)
+        end
+        scans
       end
 
       def create(data)
         AplusApi::Connection.new.post("scan", data)
       end
-
 
     end
   end
